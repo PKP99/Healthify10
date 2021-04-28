04bdb6c5b7bab7876cc2b9e53dd899ab923d9803
Paras Kumar Patel <68785857+PKP99@users.noreply.github.com>
2021-04-28 20:22:52 +0530

    loki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="shortcut icon" type="image/x-icon" src="/favicon.ico">
         <title>Healthify10</title>
     </head>
     <body>
